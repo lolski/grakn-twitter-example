8bf6e4b6eaa3e0e68a260d3eaf4ce53ff154dfa2
lolski <soulmagic13@gmail.com>
2017-06-07 19:21:13 +0400

    wip - grakn insert and match

--- src/main/java/ai/grakn/twitterexample/Main.java
+++ src/main/java/ai/grakn/twitterexample/Main.java
@@ -15,10 +15,37 @@ Goal: demonstrate streaming data into Grakn, introduce interesting Grakn concept
 // - look at other example on project format
 // - look at twitter streaming vs rest api
 
+import ai.grakn.Grakn;
+import ai.grakn.GraknGraph;
+import ai.grakn.GraknSession;
+import ai.grakn.GraknTxType;
+import ai.grakn.concept.Concept;
+import ai.grakn.graql.MatchQuery;
 import ai.grakn.graql.QueryBuilder;
+
+import java.util.Map;
+
+import static ai.grakn.graql.Graql.*;
+
 
 public class Main {
   public static void main(String[] args) {
+    GraknSession session = Grakn.session(Grakn.IN_MEMORY, "MyGraph");
+
+    // write
+
+    // select
+    GraknGraph graphReader = session.open(GraknTxType.READ);
+
+    QueryBuilder qb = graphReader.graql();
+
+    MatchQuery query = qb.match(var("x").isa("person").has("firstname", "Bob")).limit(50);
+
+    for (Map<String, Concept> result : query) {
+      System.out.println(result.get("x").getId());
+    }
+
+    graphReader.close();
     System.out.println("hello");
   }
 }
