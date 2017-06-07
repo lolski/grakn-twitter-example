d8f32e7d7db97fc54c01b3450191cbf301f3c38d
lolski <soulmagic13@gmail.com>
2017-06-07 18:35:01 +0400

    first commit

---
+++ src/main/java/ai/grakn/twitterexample/Main.java
@@ -0,0 +1,22 @@
+package ai.grakn.twitterexample;
+
+/*
+
+Goal: demonstrate streaming data into Grakn, introduce interesting Grakn concepts to the user
+- source candidate: public user tweets
+- data volume: small (small enough that we can run the program in a single node with a mid level compute power)
+  - streaming api vs rest api?
+- questions:
+  - two users who replies each other are close connections
+
+ */
+
+// TODO:
+// - look at other example on project format
+// - look at twitter streaming vs rest api
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
