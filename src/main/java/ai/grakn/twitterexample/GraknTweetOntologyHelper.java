cfb7b7a0a4896369479d5a46e23f7d956612f9c7
lolski <soulmagic13@gmail.com>
2017-06-18 17:17:01 +0400

    wip insert

--- src/main/java/ai/grakn/twitterexample/GraknTweetOntologyHelper.java
+++ src/main/java/ai/grakn/twitterexample/GraknTweetOntologyHelper.java
@@ -4,43 +4,80 @@ import ai.grakn.GraknGraph;
 import ai.grakn.GraknSession;
 import ai.grakn.GraknTxType;
 import ai.grakn.concept.*;
+import ai.grakn.graql.MatchQuery;
+import ai.grakn.graql.QueryBuilder;
+import ai.grakn.graql.admin.Answer;
 
+import java.util.Iterator;
+import java.util.Optional;
 import java.util.function.Consumer;
 
+import static ai.grakn.graql.Graql.var;
+
 public class GraknTweetOntologyHelper {
-  public static void initTweetOntology(GraknGraph graphWriter) {
+  public static void initTweetOntology(GraknGraph graknGraph) {
     // resources
-    ResourceType idType = graphWriter.putResourceType("identifier", ResourceType.DataType.STRING);
-    ResourceType textType = graphWriter.putResourceType("text", ResourceType.DataType.STRING);
-    ResourceType screenNameType = graphWriter.putResourceType("screen_name", ResourceType.DataType.STRING);
+    ResourceType idType = graknGraph.putResourceType("identifier", ResourceType.DataType.STRING);
+    ResourceType textType = graknGraph.putResourceType("text", ResourceType.DataType.STRING);
+    ResourceType screenNameType = graknGraph.putResourceType("screen_name", ResourceType.DataType.STRING);
 
     // entities
-    EntityType tweetType = graphWriter.putEntityType("tweet");
-    EntityType userType = graphWriter.putEntityType("user");
+    EntityType tweetType = graknGraph.putEntityType("tweet");
+    EntityType userType = graknGraph.putEntityType("user");
 
     // roles
-    RoleType writesType = graphWriter.putRoleType("writes");
-    RoleType writtenType = graphWriter.putRoleType("written");
+    RoleType writesType = graknGraph.putRoleType("writes");
+    RoleType writtenByType = graknGraph.putRoleType("written_by");
 
     // relations
-    RelationType tweetedType = graphWriter.putRelationType("tweeted").relates(writesType).relates(writtenType);
+    RelationType tweetedType = graknGraph.putRelationType("insertTweetedRelation").relates(writesType).relates(writtenByType);
 
     // resource and relation assignments
     tweetType.resource(idType);
     tweetType.resource(textType);
     userType.resource(screenNameType);
     userType.plays(writesType);
-    tweetType.plays(writtenType);
+    tweetType.plays(writtenByType);
   }
 
-  public static Entity insertTweet(GraknGraph graphWriter, String tweet) {
-    EntityType tweetEntityType = graphWriter.getEntityType("tweet");
-    ResourceType tweetResouceType = graphWriter.getResourceType("text");
+  public static Optional<Entity> findUser(QueryBuilder queryBuilder, String user) {
+    MatchQuery findUser = queryBuilder.match(var("x").isa("user").has("screen_name", user)).limit(1);
+    Iterator<Concept> concepts = findUser.get("x").iterator();
+    if (concepts.hasNext()) {
+      Entity entity = concepts.next().asEntity();
+      return Optional.of(entity);
+    }
+    else return Optional.empty();
+  }
+
+  public static Entity insertUser(GraknGraph graknGraph, String user) {
+    EntityType userEntityType = graknGraph.getEntityType("user");
+    ResourceType userResourceType = graknGraph.getResourceType("screen_name");
+    Entity userEntity = userEntityType.addEntity();
+    Resource userResource = userResourceType.putResource(user);
+    return userEntity.resource(userResource);
+  }
+
+  public static Entity insertUserIfNotExist(GraknGraph graknGraph, String screenName) {
+    QueryBuilder qb = graknGraph.graql();
+    return findUser(qb, screenName).orElse(insertUser(graknGraph, screenName));
+  }
+
+  public static Entity insertTweet(GraknGraph graknGraph, String tweet) {
+    EntityType tweetEntityType = graknGraph.getEntityType("tweet");
+    ResourceType tweetResouceType = graknGraph.getResourceType("text");
 
     Entity tweetEntity = tweetEntityType.addEntity();
-    Resource tweetResouce = tweetResouceType.putResource(tweet);
+    Resource tweetResource = tweetResouceType.putResource(tweet);
 
-    return tweetEntity.resource(tweetResouce);
+    return tweetEntity.resource(tweetResource);
+  }
+
+  public static Entity insertTweetedRelation(GraknGraph graknGraph, Entity user, Entity tweet) {
+//    RelationType tweetedType = graknGraph.getRelationType("insertTweetedRelation");
+//    Relation tweetedRelation = tweetedType.addRelation();
+//    tweetedRelation.
+    return null;
   }
 
   public static void withAutoCommit(GraknSession session, Consumer<GraknGraph> fn) {
