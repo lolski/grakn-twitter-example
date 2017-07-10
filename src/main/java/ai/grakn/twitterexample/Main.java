d56f70e1ab9ddc2d7c4a1d9c6522728c74c6966b
lolski <soulmagic13@gmail.com>
2017-07-10 16:57:15 +0700

    remove consumer key

--- src/main/java/ai/grakn/twitterexample/Main.java
+++ src/main/java/ai/grakn/twitterexample/Main.java
@@ -11,10 +11,10 @@ import static ai.grakn.twitterexample.AsyncTweetStreamProcessorHelper.*;
 
 public class Main {
   // Twitter credentials
-  private static final String consumerKey = "s81rBRQWHvGE1llHPYry7zSOm";
-  private static final String consumerSecret = "weQ8oZhBDZq9PjADlZJ897MAlxkXlNsUEH04jsqYPaLX4QCTKB";
-  private static final String accessToken = "1425775171-bnAiy4iF6y2SH1WMXPQmuwDm40zLTkBLk62qjxS";
-  private static final String accessTokenSecret = "XZ1SYSBOOFIH2jP6IKT3Je10tGtGKxstdPhuy2X4dHCUC";
+  private static final String consumerKey = "";
+  private static final String consumerSecret = "";
+  private static final String accessToken = "";
+  private static final String accessTokenSecret = "";
 
   // Grakn settings
   private static final String graphImplementation = Grakn.IN_MEMORY;
